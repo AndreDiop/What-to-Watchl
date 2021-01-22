12620d4867ebbe89030afc379d3ebe1017b5c2d3
Andre Diop <itsjustandre@gmail.com>
2021-01-22 13:04:25 -0500

    Build view routes
    - added server listening
    - 4 routes for movies created

--- server.js
+++ server.js
@@ -12,21 +12,40 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "trapstar",
-    database: "moviePlannerDB"
-  });
-  
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "trapstar",
+  database: "moviePlannerDB",
+});
 
-  connection.connect(function(err) {
-    if (err) {
-      console.error("error connecting: " + err.stack);
-      return;
-    }
-  
-    console.log("connected as id " + connection.threadId);
-  });
+connection.connect(function (err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+
+  console.log("connected as id " + connection.threadId);
+});
+
+// view routes
+app.get("/", (req, res) => {
+  res.send("all my movies go here");
+});
+app.get("/movies/:id", (req, res) => {
+  res.send("single movie goes here");
+});
+app.get("/movies/:id/edit", (req, res) => {
+  res.send("form to update movie here");
+});
+app.get("/movies/new", (req, res) => {
+  res.send("aform to create new movie here");
+});
 
 
+// API Routes
+
+app.listen(PORT, function () {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
